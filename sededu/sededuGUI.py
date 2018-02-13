935f9a6eb41f6b3714df75e60c1f3e7fd46c42d8
amoodie <amoodie@rice.edu>
2018-02-12 22:26:13 -0600

    GUI development

--- sededu/sededuGUI.py
+++ sededu/sededuGUI.py
@@ -6,117 +6,106 @@ from PyQt5 import QtGui, QtCore
 from sededu import guiUtils as gui
 # from sededu import src
 
-class rootInit(QWidget):
+class rootInit(QMainWindow):
     # determine path to private
     thisDir = os.path.dirname(__file__)
     thisPath = os.path.join(thisDir,'')
 
     def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
+        # QWidget.__init__(self, parent)
+        QMainWindow.__init__(self)
+        self.root = QWidget()
+        self.stack = QStackedWidget()
+        rootLayout = QVBoxLayout()
+        rootLayout.addWidget(self.stack)
+        self.root.setLayout(rootLayout)
+        self.setCentralWidget(self.root)
+
         self.initializeGUI()
+
 
     def initializeGUI(self):
         self.main = mainMenu(self) # build mainMenu
-        self.drawMain(self.main.layout) # set to mainMenu
-        self.setGeometry(400, 400, 100, 300)
-        # navTest = categoryMenu()
+        # build all categoryMenus here
+        # # #
+        # # #
+        self.riversMenu = categoryMenu("rivers")
+        self.stack.addWidget(self.main)
+        self.stack.addWidget(self.riversMenu)
     
-    def drawMain(self, mainLayout):
-        self.setLayout(mainLayout)
+    def drawMain(self):
+        # self.setLayout(mainLayout)
+        self.stack.setCurrentIndex(0)
 
-    def drawNav(self, navLayout):
-        sender = self.sender()
-        print(navLayout)
-        print(sender.text)
-        self.setLayout(navLayout)
-
+    def drawNav(self):
+        # sender = self.sender()
+        # print(navLayout)
+        # print(sender.text)
+        # self.setLayout(navLayout)
+        self.stack.setCurrentIndex(1)
 
     def genNav(self, navFolder):
         # generate nav menu from folder information
         a = 1
 
 
-
-def mainMenu(self):
+class mainMenu(QWidget):
     # class for main menu
-    # def __init__(self, parent=None):
-        # QWidget.__init__(self, parent)
-        # super().__init__()
-    mainList = ["Rivers", "Deltas", "Deserts", "Coasts", 
-        "Stratigraphy", "Behind the \nModules"] # read these from file?
-    mainN = range(len(mainList))
-    mainLayout = QGridLayout() # layout for entire main menu
-    
-    navBox = QGroupBox() # category navigation box, group title here
-    navLayout = gui.stdObjs.mainLayout(mainList, thisPath)
-    navBox.setLayout(navLayout)
-
-    etcBox = QGroupBox() # etc box, group title here
-    etcLayout = QGridLayout()
-    etcQuit = gui.stdObjs.etcButton("Quit")
-    etcQuit.clicked.connect(QtCore.QCoreApplication.instance().quit)
-    etcAbout = gui.stdObjs.etcButton("About")
-    # navTest = categoryMenu(self)
-    etcAbout.clicked.connect(self.drawNav)
-
-    etcLayout.addWidget(etcQuit, 0, 0)
-    etcLayout.addWidget(etcAbout, 0, 1)
-    etcBox.setLayout(etcLayout)
-
-    self.layout = QGridLayout() #
-    self.layout.addWidget(navBox, 0, 0, 1, 2)
-    self.layout.addWidget(etcBox, 2, 0)
-    return self
-
-
-
-# class mainMenu(QWidget):
-#     # class for main menu
-#     def __init__(self, parent=None):
-#         QWidget.__init__(self, parent)
-#         # super().__init__()
-#         mainList = ["Rivers", "Deltas", "Deserts", "Coasts", 
-#             "Stratigraphy", "Behind the \nModules"] # read these from file?
-#         mainN = range(len(mainList))
-#         mainLayout = QGridLayout() # layout for entire main menu
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        super().__init__()
+        print(dir(self))
+        mainList = ["Rivers", "Deltas", "Deserts", "Coasts", 
+            "Stratigraphy", "Behind the \nModules"] # read these from file?
+        mainN = range(len(mainList))
+        mainLayout = QGridLayout() # layout for entire main menu
         
-#         navBox = QGroupBox() # category navigation box, group title here
-#         navLayout = gui.stdObjs.mainLayout(mainList, thisPath)
-#         navBox.setLayout(navLayout)
+        navBox = QGroupBox() # category navigation box, group title here
+        navLayout = gui.stdObjs.mainLayout(mainList, thisPath)
+        navBox.setLayout(navLayout)
 
-#         etcBox = QGroupBox() # etc box, group title here
-#         etcLayout = QGridLayout()
-#         etcQuit = gui.stdObjs.etcButton("Quit")
-#         etcQuit.clicked.connect(QtCore.QCoreApplication.instance().quit)
-#         etcAbout = gui.stdObjs.etcButton("About")
-#         etcAbout.clicked.connect(self.drawNav(navTest.layout))
+        etcBox = QGroupBox() # etc box, group title here
+        etcLayout = QGridLayout()
+        etcQuit = gui.stdObjs.etcButton("Quit")
+        etcQuit.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        etcAbout = gui.stdObjs.etcButton("About")
+        etcAbout.clicked.connect(self.drawNav())
 
-#         etcLayout.addWidget(etcQuit, 0, 0)
-#         etcLayout.addWidget(etcAbout, 0, 1)
-#         etcBox.setLayout(etcLayout)
+        etcLayout.addWidget(etcQuit, 0, 0)
+        etcLayout.addWidget(etcAbout, 0, 1)
+        etcBox.setLayout(etcLayout)
 
-#         self.layout = QGridLayout() #
-#         self.layout.addWidget(navBox, 0, 0, 1, 2)
-#         self.layout.addWidget(etcBox, 2, 0)
+        layout = QGridLayout() #
+        layout.addWidget(navBox, 0, 0, 1, 2)
+        layout.addWidget(etcBox, 2, 0)
+        self.setLayout(layout)
 
-def categoryMenu(self):
-    # class for navigation menu
-    # def __init__(self, parent=None):
-        # QWidget.__init__(self, parent)
-    label = QLabel("test label")
-    self.layout = QGridLayout() #
-    self.layout.addWidget(label, 0, 0)
-    return self
+    def drawNav(self):
+        # sender = self.sender()
+        # print(navLayout)
+        # print(sender.text)
+        # self.setLayout(navLayout)
+        self.stack.setCurrentIndex(1)
 
-
-
-# class categoryMenu(QWidget):
+# def categoryMenu(self):
 #     # class for navigation menu
-#     def __init__(self, parent=None):
-#         QWidget.__init__(self, parent)
-#         label = QLabel("test label")
-#         self.layout = QGridLayout() #
-#         self.layout.addWidget(label, 0, 0)
+#     # def __init__(self, parent=None):
+#         # QWidget.__init__(self, parent)
+#     label = QLabel("test label")
+#     self.layout = QGridLayout() #
+#     self.layout.addWidget(label, 0, 0)
+#     return self
+
+
+
+class categoryMenu(QWidget):
+    # class for navigation menu
+    def __init__(self, category, parent=None):
+        QWidget.__init__(self, parent)
+        label = QLabel("test label" + category)
+        layout = QGridLayout() #
+        layout.addWidget(label, 0, 0)
+        self.setLayout(layout)
 
 
 
