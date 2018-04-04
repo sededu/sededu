3062ff029ddd217a9e84ecd10adfa820cdd910c5
amoodie <amoodie@rice.edu>
2018-04-04 18:08:29 -0500

    setup changes, create pull_request_checklist, update two submodules

---
+++ docs/pull_request_checklist.md
@@ -0,0 +1,21 @@
+# Pull Request checklist
+
+
+## Hot-fixes, bug-fixes, and features in SedEdu
+
+1.
+1.
+1.
+
+
+## Adding a submodule to SedEdu
+
+1.
+1. license file
+1.
+
+
+
+## Contributions to submodules
+
+1. Should be made in the respective repository of the submodule.
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='SedEdu',
-    version='0.1.0',
+    version='0.9.0',
     author='Andrew J. Moodie other contributors',
     author_email='amoodie@rice.edu',
     packages=['sededu'],
     url='https://github.com/amoodie/sededu',
-    license='LICENSE.md',
+    license='LICENSE.txt',
     description='sediment-related educational activity suite',
     long_description=open('README.md').read(),
 	# install_requires=[
