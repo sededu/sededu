b47d92fadc60f84f145bbac5d59a3a89a2789be2
amoodie <amoodie@rice.edu>
2018-04-09 22:26:54 -0500

    increment version nums

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from sededu
 
 setup(
     name='SedEdu',
-    version='0.9.7',
+    version='0.9.8',
     author='Andrew J. Moodie other contributors',
     author_email='amoodie@rice.edu',
     packages=['sededu'],
