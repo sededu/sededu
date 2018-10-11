91ac500dba677d4887818387d0de7cb41056d977
amoodie <amoodie@rice.edu>
2018-10-11 11:59:17 -0500

    revert build recipe

--- .ci/conda-recipe/bld.bat
+++ .ci/conda-recipe/bld.bat
@@ -1,4 +1,3 @@
 xcopy /e "%RECIPE_DIR%\.." "%SRC_DIR%"
-rem "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-"%PYTHON%" setup.py install --record record.txt
+"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
