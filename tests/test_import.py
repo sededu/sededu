c63f7f7788c927042fca72aae0ca000bb345ad99
amoodie <amoodie@rice.edu>
2018-10-11 14:00:55 -0500

    add test for _readme.json

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,3 +5,6 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 
 def test_import():
     from sededu.root import Runner
+
+def test_readme_parsed_file_exists():
+    assert os.path.isfile(os.path.join('sededu', '_readme.json'))
