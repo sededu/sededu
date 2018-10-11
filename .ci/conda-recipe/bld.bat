1df2b67e8f82fcf80a2bed2f382fde21ddb956dc
amoodie <amoodie@rice.edu>
2018-10-11 11:33:43 -0500

    change recipe, remove --single-version-externally-managed

--- .ci/conda-recipe/bld.bat
+++ .ci/conda-recipe/bld.bat
@@ -1,3 +1,4 @@
 xcopy /e "%RECIPE_DIR%\.." "%SRC_DIR%"
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+rem "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
+"%PYTHON%" setup.py install --record record.txt
 if errorlevel 1 exit 1
--- .ci/conda-recipe/build.sh
+++ .ci/conda-recipe/build.sh
@@ -1,2 +1,3 @@
 cp -r $RECIPE_DIR/.. $SRC_DIR
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
+# $PYTHON setup.py install --single-version-externally-managed --record record.txt
+$PYTHON setup.py install --record record.txt
