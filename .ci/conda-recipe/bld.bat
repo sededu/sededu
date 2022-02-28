1ac06ac741177bc95977739c8d766609920fca1e
Andrew Moodie <amoodie@utexas.edu>
2022-02-28 17:42:45 -0600

    delete old ci scripts and configs.

--- .ci/conda-recipe/bld.bat
+++
@@ -1,3 +0,0 @@
-xcopy /e "%RECIPE_DIR%\.." "%SRC_DIR%"
-"%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
-if errorlevel 1 exit 1
--- .ci/conda-recipe/build.sh
+++
@@ -1,2 +0,0 @@
-cp -r $RECIPE_DIR/.. $SRC_DIR
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
